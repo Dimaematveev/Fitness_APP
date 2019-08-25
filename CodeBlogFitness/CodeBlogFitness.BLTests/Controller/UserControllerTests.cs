9ba99a9b857447c885486ea2dbbef94e662dd86d
dimaematveev <dimaematveev@gmail.com>
2019-08-25 22:00:44 +0300

    2-й тест сделан!

--- CodeBlogFitness/CodeBlogFitness.BLTests/Controller/UserControllerTests.cs
+++ CodeBlogFitness/CodeBlogFitness.BLTests/Controller/UserControllerTests.cs
@@ -16,7 +16,23 @@ namespace CodeBlogFitness.BL.Controller.Tests
         [TestMethod()]
         public void SetNewUserDataTest()
         {
-            Assert.Fail();
+            //Arrange Объявление
+            string userName = Guid.NewGuid().ToString();
+            var gender = "man";
+            var birthDate = DateTime.Now.AddYears(-18);
+            var weight = 90;
+            var height = 190;
+            var userController = new UserController(userName);
+
+            //Act действие
+            userController.SetNewUserData(gender, birthDate, weight, height);
+            var userController2 = new UserController(userName);
+            //Assert Проверка
+            Assert.AreEqual(userName, userController2.CurrentUser.Name);
+            Assert.AreEqual(gender, userController2.CurrentUser.Gender.Name);
+            Assert.AreEqual(birthDate, userController2.CurrentUser.BirthDate);
+            Assert.AreEqual(weight, userController2.CurrentUser.Weight);
+            Assert.AreEqual(height, userController2.CurrentUser.Height);
         }
 
         [TestMethod()]
