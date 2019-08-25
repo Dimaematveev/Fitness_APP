99110e905b4a6c449c196a4915009eeb8ff1605a
dimaematveev <dimaematveev@gmail.com>
2019-08-25 16:07:12 +0300

    Добавил класс Пользователя и Гендера!

---
+++ CodeBlogFitness/CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class Gender
+    {
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException($"Имя пола не должно быть пустым или null!",nameof(name));
+            }
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
