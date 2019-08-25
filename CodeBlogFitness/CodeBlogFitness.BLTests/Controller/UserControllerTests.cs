e8fad055beb5324c188040fe71fa9709c14f255a
dimaematveev <dimaematveev@gmail.com>
2019-08-25 20:52:24 +0300

    Пустые тесты!

---
+++ CodeBlogFitness/CodeBlogFitness.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBlogFitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void UserControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
