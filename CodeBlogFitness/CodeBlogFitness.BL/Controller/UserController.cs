1de27ab3c67f7e3950c13e86d108de0f6fa56707
dimaematveev <dimaematveev@gmail.com>
2019-08-25 16:27:17 +0300

    Делаем конроллер для пользователя. Возможнось сохранения.

---
+++ CodeBlogFitness/CodeBlogFitness.BL/Controller/UserController.cs
@@ -0,0 +1,26 @@
+﻿using CodeBlogFitness.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Controller
+{
+    public class UserController
+    {
+        public User User { get; }
+        public UserController (User user)
+        {
+            if (user is null)
+            {
+                throw new ArgumentNullException("Пользователь не может быть равен null!",nameof(user));
+            }
+            User = user;
+        }
+        public bool Save()
+        {
+
+        }
+    }
+}
