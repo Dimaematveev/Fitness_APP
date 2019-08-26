5391fdb5262ca0b3a3bb680933b120ac4490f315
dimaematveev <dimaematveev@gmail.com>
2019-08-26 16:20:02 +0300

    Еда переделали!

--- CodeBlogFitness/CodeBlogFitness.BL/Model/Food.cs
+++ CodeBlogFitness/CodeBlogFitness.BL/Model/Food.cs
@@ -12,61 +12,46 @@ namespace CodeBlogFitness.BL.Model
     public class Food
     {
         /// <summary>
-        /// Название за 100гр.
+        /// Название за 1гр.
         /// </summary>
         public string Name { get;  }
         /// <summary>
-        /// Белки за 100гр.
+        /// Белки за 1гр.
         /// </summary>
         public double Proteins { get;  }
         /// <summary>
-        /// Жиры за 100гр.
+        /// Жиры за 1гр.
         /// </summary>
         public double Fats { get; }
         /// <summary>
-        /// Углеводы за 100гр.
+        /// Углеводы за 1гр.
         /// </summary>
         public double Carbohydrates { get; }
 
         /// <summary>
-        /// Калории за 100гр.
-        /// </summary>
-        public double Calorie { get; }
-        /// <summary>
         /// Калории за 1гр.
         /// </summary>
-        private double CalloriesOneGramm { get { return Calorie / 100.0; } }
-        /// <summary>
-        /// Белки за 1гр.
-        /// </summary>
-        private double ProteinsOneGramm { get { return Proteins / 100.0; } }
-        /// <summary>
-        /// Жиры за 1гр.
-        /// </summary>
-        private double FatsOneGramm { get { return Fats / 100.0; } }
-        /// <summary>
-        /// Углеводы за 1гр.
-        /// </summary>
-        private double CarbohydratesOneGramm { get { return Carbohydrates / 100.0; } }
+        public double Calorie { get; }
 
+        public Food(string name) : this(name, 0, 0, 0, 0) { }
 
-        public Food(string name)
-        {
-            //TODO: проверка
-
-            Name = name;
-        }
-
-        public Food(string name, 
+        public Food(string name,
+                    double calorie,
                     double proteins,
                     double fats,
                     double carbohydrates)
         {
             //TODO: проверка
             Name = name;
-            Proteins = proteins;
-            Fats = fats;
-            Carbohydrates = carbohydrates;
+            Calorie = calorie / 100.0;
+            Proteins = proteins / 100.0;
+            Fats = fats / 100.0;
+            Carbohydrates = carbohydrates / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Name; 
         }
     }
 }
