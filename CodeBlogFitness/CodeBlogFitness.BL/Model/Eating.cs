6f5ede5c739d3bae41cca60a35e2bbd605c9370a
dimaematveev <dimaematveev@gmail.com>
2019-08-26 16:31:09 +0300

    Сделали класс Приема пищи!

---
+++ CodeBlogFitness/CodeBlogFitness.BL/Model/Eating.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Прием пищи.
+    /// </summary>
+    public class Eating
+    {
+        /// <summary>
+        /// Момент приема пищи.
+        /// </summary>
+        public DateTime Moment { get; }
+        /// <summary>
+        /// Список продуктов приема!
+        /// </summary>
+        public Dictionary<Food, double> Foods { get; }
+        
+        /// <summary>
+        /// Кто ел.
+        /// </summary>
+        public User User { get; }
+
+        public Eating(User user)
+        {
+
+            User = user ?? throw new ArgumentNullException("Пользователь не может быть пустым!",nameof(user));
+            Moment = DateTime.UtcNow;
+            Foods = new Dictionary<Food, double>();
+        }
+        /// <summary>
+        /// Добавить продукт что съели и сколько!
+        /// </summary>
+        /// <param name="food">Продукт.</param>
+        /// <param name="weight">Вес в граммах.</param>
+        public void Add(Food food,
+                        double weight)
+        {
+            var product = Foods.Keys.FirstOrDefault(f => f.Name.Equals(food.Name));
+            if (product==null)
+            {
+                Foods.Add(food, weight);
+            }
+            else
+            {
+                Foods[product] += weight;
+            }
+
+        }
+    }
+}
