c0d4d016fa5392ca777c2a659435b40312a5d29d
dimaematveev <dimaematveev@gmail.com>
2019-08-26 17:10:01 +0300

    Создали пустой м тест!

---
+++ CodeBlogFitness/CodeBlogFitness.BLTests/Controller/EatingControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBlogFitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Controller.Tests
+{
+    [TestClass()]
+    public class EatingControllerTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
