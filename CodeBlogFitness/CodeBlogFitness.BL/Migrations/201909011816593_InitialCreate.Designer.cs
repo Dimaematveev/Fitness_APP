01f77dd3f7d4f2372dac5141bfc6d94227bfa077
dimaematveev <dimaematveev@gmail.com>
2019-09-01 21:39:17 +0300

    бд не работает((

--- CodeBlogFitness/CodeBlogFitness.BL/Migrations/201909011816593_InitialCreate.Designer.cs
+++ CodeBlogFitness/CodeBlogFitness.BL/Migrations/201909011816593_InitialCreate.Designer.cs
@@ -13,7 +13,7 @@ namespace CodeBlogFitness.BL.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201909011820390_InitialCreate"; }
+            get { return "201909011816593_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
